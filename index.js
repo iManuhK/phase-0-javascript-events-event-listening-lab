de4f097655c41d021f926a8f1589ed18936684a0
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-03-15 21:41:29 +0300

    Adds to index.js JS event listeners

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+function addingEventListener(a,b) {
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+const input = document.getElementById('button');
+
+input.addEventListener('click', clickAlert);
 }
+addingEventListener('click', clickAlert);
